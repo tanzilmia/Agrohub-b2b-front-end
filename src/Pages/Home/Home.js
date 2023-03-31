6e12deb813d49bd82de4943c38a9500d6ff4408f
alaminpk360 <alaminpkalaminpk5@gmail.com>
2023-03-31 11:07:36 +0600

    add homeProduct and

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import { myContext } from '../../contextApi/Authcontext';
+import HomeCategoryByProduct from './HomeCategoryByProduct/HomeCategoryByProduct';
+import HomeProducts from './HomeProducts/HomeProducts';
 
 const Home = () => {
     const {test} = useContext(myContext)
     return (
         <div>
+           <h1>Navbar</h1>
+           <h1>Hero section</h1>
+           <HomeProducts />
+           <HomeCategoryByProduct />
             <h2>Home Page {test} </h2>
         </div>
     );
