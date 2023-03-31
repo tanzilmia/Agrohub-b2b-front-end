c70efdaa453db33a284aa0afe0c4aa0c6ec8b915
Istiak Ahmed <108383926+Istiakahmed180@users.noreply.github.com>
2023-03-31 14:54:30 +0600

    Navbar Complete

--- src/sheardComponent/Navbar.js
+++ src/sheardComponent/Navbar.js
@@ -5,7 +5,7 @@ import header_logo from "../Assets/Images/header-logo.jpg";
 const Navbar = () => {
   return (
     <div>
-      {/* header section */}
+      {/* header section start*/}
       <header className="py-4 shadow-sm ">
         <div className="container flex items-center justify-around">
           {/* logo  */}
@@ -19,10 +19,10 @@ const Navbar = () => {
             </span>
             <input
               type="text"
-              className="w-full border border-pink-400 border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none"
+              className="w-full border border-[#29BA2F] border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none"
               placeholder="Search"
             />
-            <button className="bg-pink-400 border border-pink-400 text-white px-8 rounded-r-md hover:bg-transparent hover:text-pink-400 transition">
+            <button className="bg-[#29BA2F] border border-[#29BA2F] text-white px-8 rounded-r-md hover:bg-transparent hover:text-[#29BA2F] transition">
               Search
             </button>
           </div>
@@ -30,31 +30,31 @@ const Navbar = () => {
           <div className="flex items-center space-x-4">
             <Link
               to={"#"}
-              className="text-center text-gray-700 hover:text-pink-400 transition relative"
+              className="text-center text-gray-700 hover:text-[#29BA2F] transition relative"
             >
               <div className="text-2xl">
                 <i class="ri-heart-line"></i>
               </div>
               <div className="text-xs leading-3">Wish List</div>
-              <span className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-pink-400 text-white text-xs">
+              <span className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
                 8
               </span>
             </Link>
             <Link
               to={"#"}
-              className="text-center text-gray-700 hover:text-pink-400 transition relative"
+              className="text-center text-gray-700 hover:text-[#29BA2F] transition relative"
             >
               <div className="text-2xl">
                 <i class="ri-shopping-cart-2-line"></i>
               </div>
               <div className="text-xs leading-3">Cart</div>
-              <span className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-pink-400 text-white text-xs">
+              <span className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
                 8
               </span>
             </Link>
             <Link
               to={"#"}
-              className="text-center text-gray-700 hover:text-pink-400 transition relative"
+              className="text-center text-gray-700 hover:text-[#29BA2F] transition relative"
             >
               <div className="text-2xl">
                 <i class="ri-user-3-line"></i>
@@ -64,6 +64,91 @@ const Navbar = () => {
           </div>
         </div>
       </header>
+      {/* header section start*/}
+
+      {/* navbar section start */}
+      <div className="bg-white">
+        <div className="container mx-auto flex ">
+          {/* all category start */}
+          <div className="px-8 py-4 bg-[#29BA2F] flex items-center cursor-pointer relative group">
+            <span className="text-white">
+              <i class="ri-bar-chart-horizontal-fill"></i>
+            </span>
+            <span className="capitalize ml-2 text-white ">All Categories</span>
+            <div className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-500 invisible group-hover:visible">
+              <Link
+                to={""}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <span className="ml-6 text-gray-600 text-sm">Electronics</span>
+              </Link>
+              <Link
+                to={""}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <span className="ml-6 text-gray-600 text-sm">TV</span>
+              </Link>
+              <Link
+                to={""}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <span className="ml-6 text-gray-600 text-sm">Sports</span>
+              </Link>
+              <Link
+                to={""}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <span className="ml-6 text-gray-600 text-sm">Motorbike</span>
+              </Link>
+              <Link
+                to={""}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <span className="ml-6 text-gray-600 text-sm">Sofa</span>
+              </Link>
+            </div>
+          </div>
+          {/* all category end */}
+
+          {/* navbar links start*/}
+          <div className="flex items-center justify-between flex-grow pl-12">
+            <div className="flex items-center space-x-6 capitalize">
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                Home
+              </Link>
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                Shop
+              </Link>
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                About US
+              </Link>
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                Contact US
+              </Link>
+            </div>
+            <Link
+              to={""}
+              className=" text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+            >
+              Login/Register
+            </Link>
+          </div>
+          {/* navbar links end*/}
+        </div>
+      </div>
+      {/* navbar section end */}
     </div>
   );
 };
