f5211d2c9f6e2873149acef62d2cf9f3d0309ece
mahiuddiinnTuhin <mahiuddiinntuhin@gmail.com>
2023-03-31 17:23:58 +0600

    footer done-tuhin

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,20 @@
-import React, { useContext } from 'react';
-import { myContext } from '../../contextApi/Authcontext';
-import HomeCategoryByProduct from './HomeCategoryByProduct/HomeCategoryByProduct';
-import HomeProducts from './HomeProducts/HomeProducts';
+import React, { useContext } from "react";
+import { myContext } from "../../contextApi/Authcontext";
+import OfferFlashCard from "./flashCard/OfferFlashCard";
+import HomeCategoryByProduct from "./HomeCategoryByProduct/HomeCategoryByProduct";
+import HomeProducts from "./HomeProducts/HomeProducts";
 
 const Home = () => {
-    const {test} = useContext(myContext)
-    return (
-        <div>
-           <h1>Hero section</h1>
-           <HomeProducts />
-           <HomeCategoryByProduct />
-            {/* <h2>Home Page {test} </h2> */}
-        </div>
-    );
+  const { test } = useContext(myContext);
+  return (
+    <div>
+      <h1>Hero section</h1>
+      <HomeProducts />
+      <HomeCategoryByProduct />
+      {/* <OfferFlashCard /> */}
+      {/* <h2>Home Page {test} </h2> */}
+    </div>
+  );
 };
 
 export default Home;
