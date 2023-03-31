42e26879a1334357d480a55ef5d44774554c1447
mahiuddiinnTuhin <mahiuddiinntuhin@gmail.com>
2023-03-31 21:27:24 +0600

    flassh card and footer responsive

--- src/sheardComponent/Footer.js
+++ src/sheardComponent/Footer.js
@@ -1,198 +1,138 @@
 /* 
-    Project: AgroHub
-    Name: MD. Mahiuddin Tuhin
-    Role: Footer Making
-    Timestamp: 21/3/23 - 4.02pm
+            @Project: Agrohub (b2b website)
+            @Name: MD. Mahiuddin Tuhin
+            @Task: Making a footer components
+            @timestap: 31/3/23 - Friday - 4.30pm
 */
-
-import header_logo from "../Assets/Images/header-logo.jpg";
 import { Link } from "react-router-dom";
-
+import header_logo from "../Assets/Images/header-logo.jpg";
 export default function Footer() {
-  // using an array of object to display footer section by mapping
-  const footerNavs = [
-    {
-      label: "Company",
-      items: [
-        {
-          to: "/",
-          name: "Partners",
-        },
-        {
-          to: "/",
-          name: "Blog",
-        },
-        {
-          to: "/",
-          name: "Team",
-        },
-        {
-          to: "/",
-          name: "Careers",
-        },
-      ],
-    },
-    {
-      label: "Resources",
-      items: [
-        {
-          to: "/",
-          name: "contact",
-        },
-        {
-          to: "/",
-          name: "Support",
-        },
-        {
-          to: "/",
-          name: "Docs",
-        },
-        {
-          to: "/",
-          name: "Pricing",
-        },
-      ],
-    },
-    {
-      label: "About",
-      items: [
-        {
-          to: "/",
-          name: "Terms",
-        },
-        {
-          to: "/",
-          name: "License",
-        },
-        {
-          to: "/",
-          name: "Privacy",
-        },
-        {
-          to: "/",
-          name: "About US",
-        },
-      ],
-    },
-  ];
-
   return (
-    <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto md:px-8">
-      <div className="gap-6 justify-between md:flex">
-        {/* footer detail and subscription part */}
-
-        <div className="flex-1">
-          <div className="max-w-xs">
-            {/* footer logo */}
+    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="sm:col-span-2">
+          <Link to={"/"}>
             <img src={header_logo} alt="" className="w-32" />
-            {/* footer logo ends */}
-
-            <p className="leading-relaxed mt-2 text-[15px]">
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s.
+          </Link>
+          <div className="mt-6 lg:max-w-sm">
+            <p className="text-sm text-gray-800">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam.
+            </p>
+            <p className="mt-4 text-sm text-gray-800">
+              Eaque ipsa quae ab illo inventore veritatis et quasi architecto
+              beatae vitae dicta sunt explicabo.
             </p>
           </div>
-
-          {/* form for subscriptions */}
-          <form onSubmit={(e) => e.preventDefault()}>
-            <label className="block pt-4 pb-2">Stay up to date</label>
-            <div className="max-w-sm flex items-center border rounded-md p-1">
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="w-full p-2.5 outline-none"
-              />
-              <button className="p-2.5 rounded-md text-white bg-indigo-600 outline-none shadow-md focus:shadow-none sm:px-5">
-                Subscribe
-              </button>
-            </div>
-          </form>
         </div>
-        {/* footer detail and subscription part ends*/}
-
-        {/* footer link display part */}
-        <div className="flex-1 mt-10 space-y-6 items-center justify-between sm:flex md:space-y-0 md:mt-0">
-          {/* mapping footer footerNavs with multi map*/}
-          {footerNavs.map((item, idx) => (
-            <ul className="space-y-4" key={idx}>
-              <h4 className="text-gray-800 font-medium">{item.label}</h4>
-              {item.items.map((el, idx) => (
-                <li key={idx}>
-                  <Link
-                    to={el.to}
-                    className="hover:underline hover:text-indigo-600"
-                  >
-                    {el.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ))}
+        <div className="space-y-2 text-sm">
+          <p className="text-base font-bold tracking-wide text-gray-900">
+            Contacts
+          </p>
+          <div className="flex">
+            <p className="mr-1 text-gray-800">Phone:</p>
+            <a
+              href="tel:850-123-5021"
+              aria-label="Our phone"
+              title="Our phone"
+              className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              850-123-5021
+            </a>
+          </div>
+          <div className="flex">
+            <p className="mr-1 text-gray-800">Email:</p>
+            <a
+              href="mailto:info@lorem.mail"
+              aria-label="Our email"
+              title="Our email"
+              className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              info@lorem.mail
+            </a>
+          </div>
+          <div className="flex">
+            <p className="mr-1 text-gray-800">Address:</p>
+            <a
+              href="https://www.google.com/maps"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Our address"
+              title="Our address"
+              className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              312 Lovely Street, NY
+            </a>
+          </div>
         </div>
-        {/* footer link display part ends*/}
-      </div>
-
-      {/* footer last part */}
-      <div className="mt-8 py-6 border-t items-center justify-between sm:flex">
-        <div className="mt-4 sm:mt-0">
-          &copy; 2022 Agrohub UI All rights reserved.
-        </div>
-        <div className="mt-6 sm:mt-0">
-          <ul className="flex items-center space-x-4">
-            <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-              <Link to="/">
-                <svg class="svg-icon w-6 h-6 text-blue-400" viewBox="0 0 20 20">
-                  <path
-                    fill="none"
-                    d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"
-                  ></path>
-                </svg>
-              </Link>
-            </li>
-
-            <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-              <Link to="/">
-                <svg class="svg-icon w-6 h-6 text-blue-700" viewBox="0 0 20 20">
-                  <path
-                    fill="none"
-                    d="M11.344,5.71c0-0.73,0.074-1.122,1.199-1.122h1.502V1.871h-2.404c-2.886,0-3.903,1.36-3.903,3.646v1.765h-1.8V10h1.8v8.128h3.601V10h2.403l0.32-2.718h-2.724L11.344,5.71z"
-                  ></path>
-                </svg>
-              </Link>
-            </li>
-
-            <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-              <Link to="/">
-                <svg class="svg-icon w-6 h-6 text-blue-500" viewBox="0 0 20 20">
-                  <path
-                    fill="none"
-                    d="M10,2.531c-4.125,0-7.469,3.344-7.469,7.469c0,4.125,3.344,7.469,7.469,7.469c4.125,0,7.469-3.344,7.469-7.469C17.469,5.875,14.125,2.531,10,2.531 M10,3.776c1.48,0,2.84,0.519,3.908,1.384c-1.009,0.811-2.111,1.512-3.298,2.066C9.914,6.072,9.077,5.017,8.14,4.059C8.728,3.876,9.352,3.776,10,3.776 M6.903,4.606c0.962,0.93,1.82,1.969,2.53,3.112C7.707,8.364,5.849,8.734,3.902,8.75C4.264,6.976,5.382,5.481,6.903,4.606 M3.776,10c2.219,0,4.338-0.418,6.29-1.175c0.209,0.404,0.405,0.813,0.579,1.236c-2.147,0.805-3.953,2.294-5.177,4.195C4.421,13.143,3.776,11.648,3.776,10 M10,16.224c-1.337,0-2.572-0.426-3.586-1.143c1.079-1.748,2.709-3.119,4.659-3.853c0.483,1.488,0.755,3.071,0.784,4.714C11.271,16.125,10.646,16.224,10,16.224 M13.075,15.407c-0.072-1.577-0.342-3.103-0.806-4.542c0.673-0.154,1.369-0.243,2.087-0.243c0.621,0,1.22,0.085,1.807,0.203C15.902,12.791,14.728,14.465,13.075,15.407 M14.356,9.378c-0.868,0-1.708,0.116-2.515,0.313c-0.188-0.464-0.396-0.917-0.621-1.359c1.294-0.612,2.492-1.387,3.587-2.284c0.798,0.97,1.302,2.187,1.395,3.517C15.602,9.455,14.99,9.378,14.356,9.378"
-                  ></path>
-                </svg>
-              </Link>
-            </li>
-
-            <li className="w-10 h-10 border rounded-full flex items-center justify-center">
-              <Link to="/">
-                <svg class="svg-icon w-6 h-6 text-red-600" viewBox="0 0 20 20">
-                  <path
-                    fill="none"
-                    d="M9.797,2.214C9.466,2.256,9.134,2.297,8.802,2.338C8.178,2.493,7.498,2.64,6.993,2.935C5.646,3.723,4.712,4.643,4.087,6.139C3.985,6.381,3.982,6.615,3.909,6.884c-0.48,1.744,0.37,3.548,1.402,4.173c0.198,0.119,0.649,0.332,0.815,0.049c0.092-0.156,0.071-0.364,0.128-0.546c0.037-0.12,0.154-0.347,0.127-0.496c-0.046-0.255-0.319-0.416-0.434-0.62C5.715,9.027,5.703,8.658,5.59,8.101c0.009-0.075,0.017-0.149,0.026-0.224C5.65,7.254,5.755,6.805,5.948,6.362c0.564-1.301,1.47-2.025,2.931-2.458c0.327-0.097,1.25-0.252,1.734-0.149c0.289,0.05,0.577,0.099,0.866,0.149c1.048,0.33,1.811,0.938,2.218,1.888c0.256,0.591,0.33,1.725,0.154,2.483c-0.085,0.36-0.072,0.667-0.179,0.993c-0.397,1.206-0.979,2.323-2.295,2.633c-0.868,0.205-1.519-0.324-1.733-0.869c-0.06-0.151-0.161-0.418-0.101-0.671c0.229-0.978,0.56-1.854,0.815-2.831c0.243-0.931-0.218-1.665-0.943-1.837C8.513,5.478,7.816,6.312,7.579,6.858C7.39,7.292,7.276,8.093,7.426,8.672c0.047,0.183,0.269,0.674,0.23,0.844c-0.174,0.755-0.372,1.568-0.587,2.31c-0.223,0.771-0.344,1.562-0.56,2.311c-0.1,0.342-0.096,0.709-0.179,1.066v0.521c-0.075,0.33-0.019,0.916,0.051,1.242c0.045,0.209-0.027,0.467,0.076,0.621c0.002,0.111,0.017,0.135,0.052,0.199c0.319-0.01,0.758-0.848,0.917-1.094c0.304-0.467,0.584-0.967,0.816-1.514c0.208-0.494,0.241-1.039,0.408-1.566c0.12-0.379,0.292-0.824,0.331-1.24h0.025c0.066,0.229,0.306,0.395,0.485,0.52c0.56,0.4,1.525,0.77,2.573,0.523c1.188-0.281,2.133-0.838,2.755-1.664c0.457-0.609,0.804-1.313,1.07-2.112c0.131-0.392,0.158-0.826,0.256-1.241c0.241-1.043-0.082-2.298-0.384-2.981C14.847,3.35,12.902,2.17,9.797,2.214"
-                  ></path>
-                </svg>
-              </Link>
-            </li>
-          </ul>
+        <div>
+          <span className="text-base font-bold tracking-wide text-gray-900">
+            Social
+          </span>
+          <div className="flex items-center mt-1 space-x-3">
+            <a
+              href="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+              </svg>
+            </a>
+            <a
+              href="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                <circle cx="15" cy="15" r="4" />
+                <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+              </svg>
+            </a>
+            <a
+              href="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+              </svg>
+            </a>
+          </div>
+          <p className="mt-4 text-sm text-gray-500">
+            Bacon ipsum dolor amet short ribs pig sausage prosciutto chicken
+            spare ribs salami.
+          </p>
         </div>
       </div>
-      {/* footer last part ends */}
-      <style jsx>{`
-        .svg-icon path,
-        .svg-icon polygon,
-        .svg-icon rect {
-          fill: currentColor;
-        }
-      `}</style>
-    </footer>
+      <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
+        <p className="text-sm text-gray-600">
+          © Copyright 2020 Lorem Inc. All rights reserved.
+        </p>
+        <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+          <li>
+            <a
+              href="/"
+              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              F.A.Q
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              Terms &amp; Conditions
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
