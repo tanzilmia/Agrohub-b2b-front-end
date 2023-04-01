7490682e9493b32422b0554e6e8af30f9f7d4afd
Istiak Ahmed <108383926+Istiakahmed180@users.noreply.github.com>
2023-04-01 16:00:35 +0600

    sign up page complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,16 @@
+import { useFormik } from "formik";
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <nav className="bg-gray-50 min-h-screen flex items-center justify-center">
-      <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 ">
+    <nav className="bg-gray-50 min-h-screen  flex items-center justify-center">
+      <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-7xl p-5 ">
         {/* left div */}
-        <div className="md:w-full px-16">
+        <div className="md:w-[500px] px-16">
           <h2 className="font-bold text-2xl text-[#29BA2F] text-center">
             Login
           </h2>
@@ -20,11 +22,13 @@ const Login = () => {
             <input
               className="p-2 mt-8 rounded-xl border w-full"
               type="email"
+              id="email"
+              name="email"
               placeholder="Email"
             />
             <div className="relative">
               <input
-                className="p-2 mt-2 rounded-xl border w-full"
+                className="p-2 mt-4 rounded-xl border w-full"
                 type={show ? "text" : "password"}
                 placeholder="Password"
               />
@@ -37,7 +41,10 @@ const Login = () => {
                 }
               ></i>
             </div>
-            <button className="bg-[#29BA2F] rounded-xl text-white py-2 hover:scale-105 duration-300 font-bold ">
+            <button
+              type="submit"
+              className="bg-[#29BA2F] rounded-xl text-white py-2 hover:scale-105 duration-300 font-bold "
+            >
               Login
             </button>
             <p className=" text-sm border-gray-400">Forgot Your Password?</p>
@@ -49,18 +56,53 @@ const Login = () => {
             <hr className="border-gray-400" />
           </div>
 
-          <button className="bg-white hover:bg-[#29BA2F] hover:text-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center hover:scale-105 duration-300 hover:font-semibold">
-            <FcGoogle className="w-10 mr-3 h-10"></FcGoogle> Login With Google
+          <button className="bg-white hover:bg-[#29BA2F] hover:text-white border py-1 w-3/4 mx-auto rounded-xl mt-5 flex justify-center items-center hover:scale-105 duration-300 hover:font-semibold">
+            <FcGoogle className="w-8 mr-3 h-10"></FcGoogle> Login With Google
           </button>
         </div>
 
         {/* right div */}
-        <div className="w-full py-5 md:block hidden">
-          <img
-            className="rounded-2xl "
-            src="https://www.shutterstock.com/image-vector/photo-graphic-editor-on-computer-600w-1432421069.jpg"
-            alt=""
-          />
+        <div className="w-[500px] py-5 md:block hidden">
+          <h2 className="font-bold text-2xl text-[#29BA2F] text-center">
+            Create An Account
+          </h2>
+          <div className="flex items-center text-gray-500 py-2">
+            <i class="ri-bank-card-fill text-2xl ml-4"></i>
+            <span className="text-lg ml-4">
+              Save payment to view in-store purchases
+            </span>
+          </div>
+          <div className="flex items-center text-gray-500 py-2">
+            <i class="ri-star-smile-line text-2xl ml-4"></i>
+            <span className="text-lg ml-4">Redeem Rewards</span>
+          </div>
+          <div className="flex  items-center text-gray-500 py-2">
+            <i class="ri-shopping-cart-fill text-2xl ml-4"></i>
+            <span className="text-lg ml-4">Speedy Checkout</span>
+          </div>
+          <div className="flex  items-center text-gray-500 py-2">
+            <i class="ri-truck-line text-2xl ml-4"></i>
+            <span className="text-lg ml-4">
+              Easily track orders and view order history{" "}
+            </span>
+          </div>
+          <div className="flex  items-center text-gray-500 py-2">
+            <i class="ri-checkbox-blank-circle-line text-2xl ml-4"></i>
+            <span className="text-lg ml-4">Create A Regestry</span>
+          </div>
+          <div className="flex  items-center text-gray-500 py-2">
+            <i class="ri-git-repository-fill text-2xl ml-4"></i>
+            <span className="text-lg ml-4">View your desgin packages</span>
+          </div>
+          <div className="flex  items-center text-gray-500 py-2">
+            <i class="ri-heart-add-line text-2xl ml-4"></i>
+            <span className="text-lg ml-4">Manage Favorites Lists</span>
+          </div>
+          <Link to={"/register"}>
+            <button className="bg-white hover:bg-[#29BA2F] hover:text-white border py-3 w-1/2 mx-auto rounded-xl  flex justify-center items-center hover:scale-105 duration-300 hover:font-semibold ">
+              Register
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,179 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Register = () => {
-  return <div>This Is Register</div>;
+  const [show, setShow] = useState(false);
+  return (
+    <nav className="bg-gray-50 min-h-screen  flex items-center justify-center">
+      <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-7xl p-5 ">
+        <div className="md:w-[700px] px-16">
+          <h2 className="font-bold text-2xl text-[#29BA2F] text-center">
+            Register
+          </h2>
+          <p className="text-sm mt-4 text-[#29BA2F] text-center">
+            If You'r Not A Member, Easily Register
+          </p>
+
+          <form action="" className="flex flex-col gap-4 ">
+            <div className="flex justify-between">
+              <input
+                className="p-2 mt-8 rounded-xl border w-[45%]"
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder="First Name"
+              />
+              <input
+                className="p-2 mt-8 rounded-xl border w-[45%]"
+                type="text"
+                id="lastName"
+                name="lastName"
+                placeholder="Last Name"
+              />
+            </div>
+            <div className="flex justify-between">
+              <input
+                className="p-2 rounded-xl border w-[45%]"
+                type="number"
+                id="number"
+                name="number"
+                placeholder="Phone Number"
+              />
+              <label
+                for="dropzone-file"
+                class="flex items-center text-center w-[45%] bg-white border-2 border-dashed rounded-lg cursor-pointer dark:border-gray-300 "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-5 h-5 text-gray-300 dark:text-gray-500 ml-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                  />
+                </svg>
+
+                <h2 class="mx-3 text-gray-400">Profile Photo</h2>
+
+                <input id="dropzone-file" type="file" class="hidden" />
+              </label>
+            </div>
+
+            <input
+              className="p-2  rounded-xl border w-full"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+            />
+            <div className="relative">
+              <input
+                className="p-2 rounded-xl border w-full"
+                name="password"
+                id="password"
+                type={show ? "text" : "password"}
+                placeholder="Password"
+              />
+              <i
+                onClick={() => setShow(!show)}
+                className={
+                  show
+                    ? "ri-eye-line absolute top-[23%] text-gray-500 right-3 cursor-pointer"
+                    : "ri-eye-off-line absolute top-[23%] text-gray-500 right-3 cursor-pointer"
+                }
+              ></i>
+            </div>
+            <div className="relative">
+              <input
+                className="p-2 rounded-xl border w-full"
+                name="confirmPassword"
+                id="confirmPassword"
+                type={show ? "text" : "password"}
+                placeholder="Confirm Password"
+              />
+              <i
+                onClick={() => setShow(!show)}
+                className={
+                  show
+                    ? "ri-eye-line absolute top-[23%] text-gray-500 right-3 cursor-pointer"
+                    : "ri-eye-off-line absolute top-[23%] text-gray-500 right-3 cursor-pointer"
+                }
+              ></i>
+            </div>
+
+            <div className="flex items-center mx-auto">
+              <h1 className="text-sm text-gray-400">
+                Please Select Trade Role:
+              </h1>
+
+              <div class="flex items-center ml-4">
+                <div class="flex items-center mr-4">
+                  <input
+                    id="inline-radio"
+                    type="radio"
+                    value="Buyer"
+                    name="inline-radio-group"
+                  />
+                  <label
+                    for="inline-radio"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-500"
+                  >
+                    Buyer
+                  </label>
+                </div>
+                <div class="flex items-center mr-4">
+                  <input
+                    id="inline-2-radio"
+                    type="radio"
+                    value=""
+                    name="inline-radio-group"
+                  />
+                  <label
+                    for="inline-2-radio"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-500"
+                  >
+                    Seller
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="bg-[#29BA2F] rounded-xl text-white py-2 hover:scale-105 duration-300 font-bold "
+            >
+              Register
+            </button>
+            <p className=" text-lg border-gray-400 text-center text-gray-500">
+              Already have an account?{" "}
+              <Link
+                className="hover:underline hover:text-[#29BA2F]"
+                to={"/login"}
+              >
+                Login
+              </Link>{" "}
+            </p>
+          </form>
+
+          <div className="mt-10 grid grid-cols-3 items-center text-gray-400">
+            <hr className="border-gray-400" />
+            <p className="text-center text-sm">OR</p>
+            <hr className="border-gray-400" />
+          </div>
+
+          <button className="bg-white hover:bg-[#29BA2F] hover:text-white border py-1 w-3/4 mx-auto rounded-xl mt-5 flex justify-center items-center hover:scale-105 duration-300 hover:font-semibold">
+            <FcGoogle className="w-8 mr-3 h-10"></FcGoogle> Login With Google
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Register;
