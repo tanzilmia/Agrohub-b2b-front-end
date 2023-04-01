bd7e76652517fcf3ef902651f19feca65dba6930
alaminpk360 <alaminpkalaminpk5@gmail.com>
2023-04-01 17:35:26 +0600

    navbar completed 99%

--- src/sheardComponent/Navbar.js
+++ src/sheardComponent/Navbar.js
@@ -4,7 +4,7 @@ import header_logo from "../Assets/Images/header-logo.jpg";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav>
       {/* header section start*/}
       <header className="py-4 shadow-sm">
         <div className="container flex items-center justify-around">
@@ -48,9 +48,9 @@ const Navbar = () => {
                 <i className="ri-shopping-cart-2-line"></i>
               </div>
               <div className="text-xs leading-3">Cart</div>
-              <span className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
+              {/* <span className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
                 8
-              </span>
+              </span> */}
             </Link>
             <Link
               to={"#"}
@@ -65,7 +65,7 @@ const Navbar = () => {
           {/* responsive */}
           <div className="px-8 mx-2 flex xl:hidden py-3 bg-[#29BA2F]  items-center cursor-pointer relative group rounded z-10">
             <span className="text-white">
-              <i className="ri-bar-chart-horizontal-fill"></i>
+              <i class="ri-menu-fill"></i>
             </span>
             <div className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-500 invisible group-hover:visible">
               <Link
@@ -76,9 +76,9 @@ const Navbar = () => {
                   <i className="ri-heart-line"></i>
                 </div>
                 <div className="text-xs leading-3">Wish List</div>
-                <span className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
+                {/* <span className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
                   8
-                </span>
+                </span> */}
               </Link>
 
               <Link
@@ -110,10 +110,10 @@ const Navbar = () => {
 
       {/* navbar section start */}
       <div className="bg-white shadow-sm">
-        <div className="container mx-auto flex ">
+        <div className="container mx-auto flex justify-between xl:justify-around">
           {/* all category start */}
-          <div className="px-8 ml-1 py-4 bg-[#29BA2F] flex items-center cursor-pointer relative group rounded z-10">
-            <span className="text-white ml-[-30px]">
+          <div className="px-8 ml-1 xl:bg-[#29BA2F] py-4 flex items-center cursor-pointer relative group rounded z-10">
+            <span title="Category" className="text-[#29BA2F] xl:text-white">
               <i className="ri-bar-chart-horizontal-fill"></i>
             </span>
             <span className="capitalize ml-2 text-white">Categories</span>
@@ -153,34 +153,33 @@ const Navbar = () => {
           {/* all category end */}
 
           {/* navbar links start*/}
-          <section className="flex justify-center w-full">
-            <div className="xl:flex hidden items-center justify-between flex-grow pl-12">
-              <div className="flex items-center space-x-6 capitalize">
-                <Link
-                  to={"/"}
-                  className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
-                >
-                  Home
-                </Link>
-                <Link
-                  to={"/"}
-                  className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
-                >
-                  Shop
-                </Link>
-                <Link
-                  to={"/"}
-                  className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
-                >
-                  About US
-                </Link>
-                <Link
-                  to={"/"}
-                  className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
-                >
-                  Contact US
-                </Link>
-              </div>
+
+          <div className="xl:flex hidden items-center justify-end flex-grow pl-12">
+            <div className="flex justify-around items-center space-x-6 capitalize mr-4">
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                Home
+              </Link>
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                Shop
+              </Link>
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                About US
+              </Link>
+              <Link
+                to={"/"}
+                className="text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
+              >
+                Contact US
+              </Link>
               <Link
                 to={""}
                 className=" text-gray-600 hover:text-black hover:border-b-2 hover:border-b-[#29BA2F] transition"
@@ -188,54 +187,66 @@ const Navbar = () => {
                 Login/Register
               </Link>
             </div>
-            {/* responsive */}
-            <div className="px-8 mx-2 flex xl:hidden py-3 bg-[#29BA2F]  items-center cursor-pointer relative group rounded z-10">
-              <span className="text-white">
-                <i className="ri-bar-chart-horizontal-fill"></i>
-              </span>
-              <div className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-500 invisible group-hover:visible">
-                <Link
-                  to={""}
-                  className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-                >
-                  <div className="text-2xl">
-                    <i className="ri-heart-line"></i>
-                  </div>
-                  <div className="text-xs leading-3">Wish List</div>
-                  <span className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
-                    8
-                  </span>
-                </Link>
+          </div>
+          {/* responsive */}
+          <div className="px-8 mx-2 flex xl:hidden py-3  items-center cursor-pointer relative group rounded z-10">
+            <span className="text-[#29BA2F]">
+              <i class="ri-layout-masonry-fill"></i>
+            </span>
+            <div className="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-500 invisible group-hover:visible">
+              <Link
+                to={""}
+                className="flex flex-col items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <div className="text-2xl">
+                  <i class="ri-home-3-line"></i>
+                </div>
+                <div className="text-xs leading-3">Home</div>
+              </Link>
 
-                <Link
-                  to={""}
-                  className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-                >
-                  <div className="text-2xl">
-                    <i className="ri-shopping-cart-2-line"></i>
-                  </div>
-                  <div className="text-xs leading-3">Cart</div>
-                  <span className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-[#29BA2F] text-white text-xs">
-                    8
-                  </span>
-                </Link>
-                <Link
-                  to={""}
-                  className="flex items-center px-6 py-3 hover:bg-gray-100 transition"
-                >
-                  <div className="text-2xl">
-                    <i className="ri-user-3-line"></i>
-                  </div>
-                  <div className="text-xs leading-3">Account</div>
-                </Link>
-              </div>
+              <Link
+                to={""}
+                className="flex flex-col items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <div className="text-2xl">
+                  <i class="ri-shopping-bag-3-line"></i>
+                </div>
+                <div className="text-xs leading-3">Shop</div>
+              </Link>
+              <Link
+                to={""}
+                className="flex flex-col items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <div className="text-2xl">
+                  <i class="ri-home-3-line"></i>
+                </div>
+                <div className="text-xs leading-3">About US</div>
+              </Link>
+              <Link
+                to={""}
+                className="flex flex-col items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <div className="text-2xl">
+                  <i class="ri-passport-line"></i>
+                </div>
+                <div className="text-xs leading-3">Contact US</div>
+              </Link>
+              <Link
+                to={""}
+                className="flex flex-col items-center px-6 py-3 hover:bg-gray-100 transition"
+              >
+                <div className="text-2xl">
+                  <i class="ri-login-box-line"></i>
+                </div>
+                <div className="text-xs leading-3">Login/Register</div>
+              </Link>
             </div>
-          </section>
+          </div>
           {/* navbar links end*/}
         </div>
       </div>
       {/* navbar section end */}
-    </div>
+    </nav>
   );
 };
 
