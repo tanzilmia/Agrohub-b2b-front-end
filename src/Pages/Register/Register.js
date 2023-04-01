f7e6dc0519b6b7e913e0276bb7c77c82a0158bc0
Istiak Ahmed <108383926+Istiakahmed180@users.noreply.github.com>
2023-04-01 19:30:07 +0600

    sign up page profile picture

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -175,9 +175,29 @@ const Register = (props) => {
               </Link>{" "}
             </p>
           </form>
-          <aside className="flex justify-center ">
-            <ul className="w-32 ">{files}</ul>
-          </aside>
+          <div>
+            {files ? (
+              <aside className="flex justify-center ">
+                <ul className="w-32 ">{files}</ul>
+              </aside>
+            ) : (
+              <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                <svg
+                  className="absolute w-12 h-12 text-gray-400 -left-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            )}
+          </div>
+
           <div className="mt-10 grid grid-cols-3 items-center text-gray-400">
             <hr className="border-gray-400" />
             <p className="text-center text-sm">OR</p>
