0f5a479e5f4ee5f0a886ee0d8f61a8704f2a21d3
mahiuddiinnTuhin <mahiuddiinntuhin@gmail.com>
2023-04-03 04:29:58 +0600

    dashboard sidenav ui responsive

--- src/sheardComponent/Footer.js
+++ src/sheardComponent/Footer.js
@@ -6,9 +6,11 @@
 */
 import { Link } from "react-router-dom";
 import header_logo from "../Assets/Images/header-logo.jpg";
+
+// footer starts here
 export default function Footer() {
   return (
-    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div className="px-4 pt-16 md:mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
       <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
         <div className="sm:col-span-2">
           <Link to={"/"}>
