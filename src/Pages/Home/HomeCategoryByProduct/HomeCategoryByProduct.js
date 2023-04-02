ce4b8993a2d1edda4da58ae64042add9b32687ad
alaminpk360 <alaminpkalaminpk5@gmail.com>
2023-04-02 09:13:11 +0600

    category section completed

--- src/Pages/Home/HomeCategoryByProduct/HomeCategoryByProduct.js
+++ src/Pages/Home/HomeCategoryByProduct/HomeCategoryByProduct.js
@@ -3,17 +3,17 @@ import { Category } from "../../../utility/HomePageFakeData";
 
 const HomeCategoryByProduct = () => {
   return (
-    <main>
-      <h2 className="text-4xl font-bold text-center m-24 mb-20">
+    <main className="bg-gray-100">
+      <h2 className="text-4xl font-bold text-center m-24 mb-20 ">
         Shop By Category
       </h2>
       <section className="grid  place-items-center sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-5  ">
         {Category.map((categoryData) => {
           return (
-            <section className="h-72 w-60 mb-20 cursor-pointer">
+            <section className="h-72 grid place-content-center md:px-10 lg:px-20 bg-white w-60 mb-20 cursor-pointer">
               <img src={categoryData.image} alt={categoryData.name} />
 
-              <h2 className="text-xl font-semibold text-center underline text-[#29BA2F]  hover:no-underline">
+              <h2 className="text-xl font-semibold text-center  text-[#29BA2F]  ">
                 {categoryData.category}
               </h2>
             </section>
