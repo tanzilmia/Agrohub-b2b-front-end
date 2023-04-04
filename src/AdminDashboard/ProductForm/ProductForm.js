84ba8d49ff125cca519e1a7f38532e9a70eaf46c
Istiak Ahmed <108383926+Istiakahmed180@users.noreply.github.com>
2023-04-04 12:38:04 +0600

    add product create object complte

--- src/AdminDashboard/ProductForm/ProductForm.js
+++ src/AdminDashboard/ProductForm/ProductForm.js
@@ -1,167 +1,196 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ProductForm = () => {
-    const [product, setProduct] = useState([])
-    const [size, setSize] = useState([]);
-    const [brand, setBrand] = useState('')
+  const [size, setSize] = useState([]);
+  const [brand, setBrand] = useState("");
 
-    const handleSizeChange = (event) => {
-        event.preventDefault();
-        // console.log(event.target.value);
-        setBrand(event.target.value)
-        setSize([...size, event.target.value]);
-    }
-    const onSubmit = (event) => {
-        event.preventDefault();
-        const name = event.target.productName.value;
-        const price = event.target.price.value;
-        const brand = event.target.brand.value;
-        const type = event.target.type.value;
-        const image = event.target.image.files[0];
-        const formData = new FormData()
-        formData.append('image', image);
-        const details = {
-            size,
-            name,
-            formData,
-            price,
-            brand,
-            type
-        }
-        setProduct(details)
-    }
-    console.log(product);
+  const handleSizeChange = (event) => {
+    event.preventDefault();
+    setSize([...size, event.target.value]);
+  };
 
+  const handleBrand = (event) => {
+    event.preventDefault();
+    setBrand(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.productName.value;
+    const image = event.target.image.files[0];
+    const oldPrice = event.target.oldPrice.value;
+    const newPrice = event.target.newPrice.value;
+    const stock = event.target.stock.value;
+    const description = event.target.description.value;
+    const formData = new FormData();
+    formData.append("image", image);
 
-    return (
-        <div>
-            <div className="flex justify-center border border-red-500 md:w-[1000px]">
-                <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600">
-                    <h1 className="text-3xl font-semibold text-center text-indigo-700 uppercase">
-                        Post your products
-                    </h1>
-                    <form onSubmit={onSubmit} className="mt-6 md:grid md:grid-cols-2 gap-6">
-                        <div className="mb-2 col-span-1">
-                            <label>
-                                <span className="text-gray-700">Product name</span>
-                                <input
-                                    type="text"
-                                    name="productName"
-                                    className="w-full border block px-4 py-2 mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    placeholder="Type here..."
-                                />
-                            </label>
-                        </div>
-                        <div className="mb-2 col-span-1">
-                            <label>
-                                <span className="text-gray-700">Product image</span>
-                                <input
-                                    type="file"
-                                    name="image"
-                                    className="w-full border block px-4 py-2 mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    placeholder="Type here..."
-                                />
-                            </label>
-                        </div>
-                        <div className="mb-2 col-span-1">
-                            <label>
-                                <span className="text-gray-700">Old price</span>
-                                <input
-                                    name="price"
-                                    type="text"
-                                    className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    placeholder="Type here..."
-                                />
-                            </label>
-                        </div>
-                        <div className="mb-2 col-span-1">
-                            <label>
-                                <span className="text-gray-700">New price</span>
-                                <input
-                                    name="price"
-                                    type="text"
-                                    className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    placeholder="Type here..."
-                                />
-                            </label>
-                        </div>
-                        <div className="mb-2">
-                            <label>
-                                <span className="text-gray-700">Brand</span>
-                                <select className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    name="brand">
-                                    onChange={handleSizeChange}
-                                    <option value="Vivo">Vivo</option>
-                                    <option value="Samsung">Samsung</option>
-                                    <option value="Walton">Walton</option>
-                                    <option value="Nokia">Nokia</option>
-                                </select>
-                            </label>
-
-                        </div>
-                        <div className="mb-2">
-                            <label>
-                                <span className="text-gray-700">Size</span>
-                                <select
-                                    className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    name="size"
-                                    onChange={handleSizeChange}
-                                >
-                                    <option value="M">M</option>
-                                    <option value="S">S</option>
-                                    <option value="L">L</option>
-                                    <option value="XL">XL</option>
-                                </select>
-                            </label>
-                        </div>
-                        <div className="mb-2">
-                            <label>
-                                <span className="text-gray-700">Type</span>
-                                <input
-                                    name="type"
-                                    type="text"
-                                    className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    placeholder="Type here..."
-                                />
-                            </label>
-                        </div>
-                        <div className="mb-2">
-                            <label>
-                                <span className="text-gray-700">Available stock</span>
-                                <input
-                                    name="availability"
-                                    type="text"
-                                    className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    placeholder="Type here..."
-                                />
-                            </label>
-                        </div>
-                        <div className="mb-2 col-span-2">
-                            <label>
-                                <span className="text-gray-700">Description</span>
-                                <textarea
-                                    name="description"
-                                    type="text"
-                                    placeholder='Type here...'
-                                    className="block border w-full mt-2 px-4 py-8 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    rows="5"
-                                ></textarea>
-                            </label>
-                        </div>
-
-                        <div className="mb-6">
-                            <button
-                                type="submit"
-                                className="h-10 px-5 text-indigo-100 bg-indigo-700 rounded-lg transition-colors duration-150 focus:shadow-outline hover:bg-indigo-800"
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div >
-        </div >
+    fetch(
+      `https://api.imgbb.com/1/upload?key=bb1b4cc357be233d3f5e60bd68475f0b`,
+      {
+        method: "POST",
+        body: formData,
+      }
     )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          const product = {
+            name,
+            image: data.data.display_url,
+            oldPrice,
+            newPrice,
+            brand,
+            size,
+            stock,
+            description,
+          };
+          console.log(product);
+        }
+      });
+  };
+
+  return (
+    <div>
+      <div className="flex justify-center border border-red-500 md:w-[1000px]">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600">
+          <h1 className="text-3xl font-semibold text-center text-indigo-700 uppercase">
+            Post your products
+          </h1>
+          <form
+            onSubmit={onSubmit}
+            className="mt-6 md:grid md:grid-cols-2 gap-6"
+          >
+            <div className="mb-2 col-span-1">
+              <label>
+                <span className="text-gray-700">Product Name</span>
+                <input
+                  type="text"
+                  name="productName"
+                  className="w-full border block px-4 py-2 mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder="Name"
+                />
+              </label>
+            </div>
+            <div className="mb-2 col-span-1">
+              <label>
+                <span className="text-gray-700">Product Image</span>
+                <input
+                  type="file"
+                  name="image"
+                  className="w-full border block px-4 py-2 mt-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder="Type here..."
+                />
+              </label>
+            </div>
+            <div className="mb-2 col-span-1">
+              <label>
+                <span className="text-gray-700">Old Price</span>
+                <input
+                  name="oldPrice"
+                  type="text"
+                  className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder="Old Price"
+                />
+              </label>
+            </div>
+            <div className="mb-2 col-span-1">
+              <label>
+                <span className="text-gray-700">New Price</span>
+                <input
+                  name="newPrice"
+                  type="text"
+                  className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder="New Price"
+                />
+              </label>
+            </div>
+            <div className="mb-2">
+              <label>
+                <span className="text-gray-700">Brand</span>
+                <select
+                  className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  name="brand"
+                  onChange={handleBrand}
+                >
+                  <option disabled selected>
+                    Choose a brand
+                  </option>
+
+                  <option>Vivo</option>
+                  <option>Samsung</option>
+                  <option>Walton</option>
+                  <option>Nokia</option>
+                </select>
+              </label>
+            </div>
+            <div className="mb-2">
+              <label>
+                <span className="text-gray-700">Create A New Brand</span>
+                <input
+                  name="nbrand"
+                  type="text"
+                  className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder="New Brand"
+                  onChange={handleBrand}
+                />
+              </label>
+            </div>
+            <div className="mb-2">
+              <label>
+                <span className="text-gray-700">Size</span>
+                <select
+                  className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  name="size"
+                  onChange={handleSizeChange}
+                >
+                  <option disabled selected>
+                    Choose a size
+                  </option>
+
+                  <option value="M">M</option>
+                  <option value="S">S</option>
+                  <option value="L">L</option>
+                  <option value="XL">XL</option>
+                </select>
+              </label>
+            </div>
+
+            <div className="mb-2">
+              <label>
+                <span className="text-gray-700">Available stock</span>
+                <input
+                  name="stock"
+                  type="text"
+                  className="block border w-full mt-2 px-4 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder="Stock"
+                />
+              </label>
+            </div>
+            <div className="mb-2 col-span-2">
+              <label>
+                <span className="text-gray-700">Description</span>
+                <textarea
+                  name="description"
+                  type="text"
+                  placeholder="Write Here"
+                  className="block border w-full mt-2 px-4 py-8 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  rows="5"
+                ></textarea>
+              </label>
+            </div>
+
+            <div className="mb-6">
+              <button
+                type="submit"
+                className="h-10 px-5 text-indigo-100 bg-indigo-700 rounded-lg transition-colors duration-150 focus:shadow-outline hover:bg-indigo-800"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ProductForm;
-
