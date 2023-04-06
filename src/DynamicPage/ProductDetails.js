fa7b63eb07d4075246f4464ca54eb86966f44895
Istiak Ahmed <108383926+Istiakahmed180@users.noreply.github.com>
2023-04-07 00:29:40 +0600

    some desgin add

--- src/DynamicPage/ProductDetails.js
+++ src/DynamicPage/ProductDetails.js
@@ -2,7 +2,7 @@
  * @ Author: Rakibul Hasan
  * @ Create Time: 2023-04-06 00:29:27
  * @ Modified by: Your name
- * @ Modified time: 2023-04-06 23:50:38
+ * @ Modified time: 2023-04-07 00:29:10
  * @ Description: Dynamically working this component Istiak Ahmed
  */
 
@@ -36,13 +36,13 @@ const ProductDetails = ({ products }) => {
   return (
     <>
       <Navbar2 />
-      <div className="mt-20">
+      <div className="m-20">
         <div className="grid md:grid-cols-5 items-center">
           <div className="md:col-span-2 flex justify-center items-center">
             <img src={image} alt="" />
           </div>
           <div className="md:col-span-3 mx-20">
-            <h2 className="text-4xl">{name}</h2>
+            <h2 className="text-2xl font-semibold">{name}</h2>
             <div className="flex gap-4 my-3">
               <i className="ri-star-fill text-orange-600"></i>
               <i className="ri-star-fill text-orange-600"></i>
@@ -50,65 +50,84 @@ const ProductDetails = ({ products }) => {
               <i className="ri-star-fill text-orange-600"></i>
             </div>
             <div>
-              <p className="text-xl mb-4">
+              <p className=" mb-4">
                 {description ? description.slice(0, 250) : "No Description"}
               </p>
-              <div className="font-semibold flex flex-col gap-4">
-                <p className="flex flex-row">
-                  {" "}
-                  <label>EFFECTIVE PRICE: </label>{" "}
-                  <span className="ml-2"> $ {newPrice}</span>
+              <div className=" flex flex-col gap-4">
+                <p className="flex flex-row   ">
+                  <label>Price: </label>{" "}
+                  <span className="ml-2 font-semibold text-lg">
+                    {" "}
+                    <span className="text-orange-300 ">$</span> {newPrice}
+                  </span>
                 </p>
                 <p className="flex flex-row">
-                  <label>BRAND:</label>{" "}
-                  <span className="ml-2"> Cushnie et Ochs</span>
+                  <label>Brand:</label>{" "}
+                  <span className="ml-2 font-semibold"> Cushnie et Ochs</span>
                 </p>
                 <p className="flex flex-row">
-                  <label>TYPE:</label> <span className="ml-2"> Tops</span>
-                </p>
-                <p className="flex flex-row">
-                  <label>AVAILABILITY:</label>{" "}
-                  <span className="ml-2"> {stock} in stock</span>
+                  <label>Status:</label>{" "}
+                  <span className="ml-2 font-semibold">In Stock</span>
                 </p>
               </div>
             </div>
-            <div className="font-semibold flex flex-col gap-4 mt-4">
+            <div className=" flex flex-col gap-4 mt-4">
               <p className="flex items-center">
-                SIZE:
-                <span className="flex gap-6 ml-2">
+                Size:
+                <span className="flex gap-3 ml-2">
                   {size.map((sz, i) => (
-                    <p className="border-2 border-black py-2 px-3">{sz}</p>
+                    <p className="border border-black py-0 px-2 font-semibold">
+                      {sz}
+                    </p>
                   ))}
                 </span>
               </p>
-              <p className="flex items-center">
-                COLOR:
-                <span className="flex gap-6 ml-2">
-                  <p className="border-2 p-4 rounded-full bg-blue-600"></p>
-                  <p className="border-2 p-4 rounded-full bg-red-800"></p>
-                  <p className="border-2 p-4 rounded-full bg-black"></p>
-                  <p className="border-2 p-4 rounded-full bg-pink-600"></p>
-                </span>
-              </p>
-              <p className="flex items-center">
-                QUANTITY:
-                <span className="flex ml-2">
+              <p class="flex items-center">
+                <span class="mr-2">Quality:</span>
+                <span class="flex items-center border border-black rounded-full">
                   <button
                     onClick={handleDecrement}
-                    className="border border-black py-2 px-3"
+                    class="flex items-center justify-center h-8 w-8 rounded-l-full border-r border-black"
                   >
-                    -
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.293a1 1 0 00-1.414-1.414L10 8.586l-2.293-2.293a1 1 0 00-1.414 1.414L8.586 10l-2.293 2.293a1 1 0 001.414 1.414L10 11.414l2.293 2.293a1 1 0 001.414-1.414L11.414 10l2.293-2.293z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
                   </button>
-                  <p className="border border-black py-2 px-3">{count}</p>
+                  <p class="px-4 py-1 text-lg font-semibold">{count}</p>
                   <button
                     onClick={handleIncrement}
-                    className="border border-black py-2 px-3"
+                    class="flex items-center justify-center h-8 w-8 rounded-r-full border-l border-black"
                   >
-                    +
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.293a1 1 0 00-1.414-1.414L10 8.586l-2.293-2.293a1 1 0 00-1.414 1.414L8.586 10l-2.293 2.293a1 1 0 001.414 1.414L10 11.414l2.293 2.293a1 1 0 001.414-1.414L11.414 10l2.293-2.293z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
                   </button>
                 </span>
               </p>
-              <p>SUBTOTAL: $ {countPrice}</p>
+
+              <p>
+                Subtotal:{" "}
+                <span className="text-orange-300 font-semibold">$ </span>
+                <span className="font-semibold">{countPrice}</span>
+              </p>
             </div>
             <div className="flex flex-col sm:flex-row md:flex-col lg:flex-row gap-3 mt-10">
               <button className="border-2 max-w-[250px] rounded-full flex justify-center gap-2 py-2 px-3 font-semibold hover:bg-orange-500 hover:text-white">
