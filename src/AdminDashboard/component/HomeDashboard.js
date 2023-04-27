4025467c6c59788680436ec5abb77af492954ec2
Md tanzil <ziltan590@gmail.com>
2023-04-27 12:08:37 +0600

    add admin panel total sells , buyer & sellers

--- src/AdminDashboard/component/HomeDashboard.js
+++ src/AdminDashboard/component/HomeDashboard.js
@@ -1,19 +1,18 @@
-/* 
-            @Project: Agrohub (b2b website)
-            @Name: MD. Mahiuddin Tuhin
-            @Task: Making Dashboard Layout components
-            @timestap: 1/4/23 - Saturday - Morning
-*/
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BestSeller from "./tableMenu/BestSeller";
-import Charts from "./Charts";
 import TableInHome from "./tableMenu/TableInHome";
+import { FcCurrencyExchange } from 'react-icons/fc';
+import { FaShoppingCart,FaUsers } from 'react-icons/fa';
+
 
 const HomeDashboard = () => {
   const [userData, setUserData] = useState([]);
   const [buyerData, setBuyerData] = useState([]);
   const [allProductData, setAllProductData] = useState([]);
+  const [totalSell, settotalSell] = useState(null);
+  const [totalPorduct, settotalPorduct] = useState([]);
   useEffect(() => {
     const getData = async () => {
       const res = await axios.get("http://localhost:5000/common/sellers");
@@ -22,35 +21,86 @@ const HomeDashboard = () => {
       const response = await axios.get("http://localhost:5000/common/buyer");
       const resData = await response.data;
       setBuyerData(resData);
-      const productdData = await axios.get("http://localhost:5000/seller/recent_Product");
+      const productdData = await axios.get(
+        "http://localhost:5000/seller/recent_Product"
+      );
       const resProductData = productdData.data;
       setAllProductData(resProductData);
+
+      const totalProduct = await axios.get(
+        "http://localhost:5000/seller/all_Product"
+      );
+      const restotalProduct = totalProduct.data;
+      settotalPorduct(restotalProduct);
     };
     getData();
   }, []);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/payment-gateway/totalsells`)
+    .then(res => {
+      settotalSell(res.data.totalSellPrice);
+    })
+    .catch(e=> console.log(e.message))
+
+  }, [])
+  
+
   return (
     <div className="flex flex-col">
       {/* this is default home page  */}
-      <div className="flex flex-col lg:grid lg:grid-flow-col lg:grid-cols-5 items-center justify-center">
-        <div className="lg:col-span-3">
-          <Charts />
+      <div className="flex flex-col lg:grid lg:grid-flow-col lg:grid-cols-5  items-center justify-center">
+        <div className="lg:col-span-3 flex items-center mx-3 mt-5 justify-center flex-wrap">
+        <div className="flex flex-wrap justify-center">
+  <div className="w-full lg:w-1/2 py-6  px-6 shadow-lg hover:shadow-xl  mb-5  transition duration-300">
+    <div className="font-bold text-5xl text-center flex justify-center"> <FcCurrencyExchange/> </div>
+    <p className="text-center text-2xl">
+    <span className="text-base mr-2 font-bold">TOTAL</span> 
+      {totalSell} <span className="text-base font-bold">TK</span>
+    </p>
+  </div>
+  <div className="w-full lg:w-1/2 py-6  px-6 shadow-lg hover:shadow-xl  mb-5  transition duration-300">
+  <div className="font-bold text-5xl text-center text-[#6c976c] flex justify-center"> <FaShoppingCart/> </div>
+    <p className="text-center text-2xl">
+      {totalPorduct?.length} <span className="text-base">Product</span>
+    </p>
+  </div>
+  <div className="w-full lg:w-1/2 py-6  px-6 shadow-lg hover:shadow-xl transition duration-300">
+  <div className="font-bold text-5xl text-center text-[#6c976c] flex justify-center"> <FaUsers/> </div>
+    <p className="text-center text-2xl">
+      {userData?.length} <span className="text-base">Sellers</span>
+    </p>
+  </div>
+  <div className="w-full lg:w-1/2 py-6  px-6 shadow-lg hover:shadow-xl transition duration-300">
+  <div className="font-bold text-5xl text-center text-[#6c976c] flex justify-center"> <FaUsers/> </div>
+    <p className="text-center text-2xl">
+      {buyerData?.length} <span className="text-base">buyers</span>
+    </p>
+  </div>
+</div>
+
+          
+          
+        
         </div>
+
         <div className="lg:col-span-2">
           <div className="">
-            <h3 className="text-gray-800 text-xl font-bold sm:text-4xl my-12">
+            <h3 className="text-gray-800 text-xl text-center font-bold sm:text-4xl my-12">
               All Seller
             </h3>
           </div>
           <BestSeller userData={userData} />
         </div>
       </div>
+
       <div className="flex flex-col lg:grid lg:grid-flow-col lg:grid-cols-5 items-center justify-center">
         <div className="lg:col-span-3">
           <TableInHome allProductData={allProductData} />
         </div>
         <div className="lg:col-span-2">
           <div className="">
-            <h3 className="text-gray-800 text-xl font-bold sm:text-4xl my-12">
+            <h3 className="text-gray-800 text-xl text-center font-bold sm:text-4xl my-12">
               All Buyer
             </h3>
           </div>
