d17c062d06e71d01ee76dc44b26698006428b6e7
Istiakahmed180 <istiakahmed180@gmail.com>
2023-05-07 22:54:30 +0600

    populer blog and blog post design and set navbar2

--- src/DynamicPage/BlogPost.js
+++ src/DynamicPage/BlogPost.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import Navbar2 from "../sheardComponent/Navbar2";
 
 const BlogPost = () => {
   const singleBlog = useLoaderData();
   const { blogTitle, thumbnail, blogDetails, author } = singleBlog;
 
   return (
-    <div className="min-h-screen bg-gray-100">
-        <Link to ='/' className='btn text-center p-3  bg-[#4eef77]'>Back To Home</Link>
+    <div className="min-h-screen bg-gray-100 dark:bg-[#1B1B1D]">
+      <Navbar2></Navbar2>
       <div className="max-w-5xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-extrabold text-gray-900">{blogTitle}</h1>
-        <p className="mt-4 text-lg text-gray-500">  <strong>Post By</strong> {author}</p>
+        <h1 className="text-3xl font-extrabold text-gray-900 dark:text-gray-300">
+          {blogTitle}
+        </h1>
+        <p className="mt-4 text-lg text-gray-500 dark:text-gray-400">
+          {" "}
+          <strong>Post By</strong> {author}
+        </p>
         <img
           className="mt-8 rounded-lg shadow-md object-cover object-center h-64 w-full"
           src={thumbnail}
           alt={blogTitle}
         />
         <div
-          className="mt-8 prose prose-indigo prose-lg text-gray-500 mx-auto"
+          className="mt-8 prose prose-indigo prose-lg text-gray-500 mx-auto dark:text-gray-400"
           dangerouslySetInnerHTML={{ __html: blogDetails }}
         />
       </div>
--- src/Pages/Home/PopularBlogs/PopularBlogs.js
+++ src/Pages/Home/PopularBlogs/PopularBlogs.js
@@ -1,37 +1,46 @@
-import axios from 'axios';
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const PopularBlogs = () => {
+  const [popularBlog, setpopularBlog] = useState([]);
 
-    const [popularBlog, setpopularBlog] = useState([])
+  useEffect(() => {
+    try {
+      axios
+        .get(`https://agrohub-b2b-new-server.vercel.app/seller/letest-blogs`)
+        .then((res) => {
+          setpopularBlog(res.data);
+        });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }, []);
 
-    useEffect(() => {
-   try{
-    axios.get(`https://agrohub-b2b-new-server.vercel.app/seller/letest-blogs`)
-    .then(res =>{
-       setpopularBlog(res.data)
-    })
-   }catch(e){
-    console.log(e.message);
-   }
-    
-    }, [])
-    
-
-    return (
-        <>
-        <h1 className='text-center text-4xl my-5 font-semibold'>Popular Blog</h1>
-        <div className='flex flex-wrap justify-center my-10'>
-          {popularBlog.length && popularBlog?.map(blog => (
+  return (
+    <>
+      <h1 className="text-center text-4xl my-5 font-semibold dark:text-gray-300">
+        Popular Blog
+      </h1>
+      <div className="flex flex-wrap justify-center my-10">
+        {popularBlog.length &&
+          popularBlog?.map((blog) => (
             <div className="w-full sm:w-1/2 md:w-1/3 p-4" key={blog._id}>
-              <div className="h-full bg-white shadow-md rounded-md overflow-hidden">
-                <img className="w-full h-40 object-cover" src={blog?.thumbnail} alt={blog?.blogTitle} />
+              <div className="h-full bg-white dark:bg-[#4B5563] shadow-md rounded-md overflow-hidden">
+                <img
+                  className="w-full h-40 object-cover"
+                  src={blog?.thumbnail}
+                  alt={blog?.blogTitle}
+                />
                 <div className="p-4">
-                  <h3 className="text-lg font-medium mb-2">{blog?.blogTitle}</h3>
-                  <p className="text-gray-500">{blog?.blogDetails?.slice(0, 100)}...</p>
+                  <h3 className="text-lg font-medium mb-2 dark:text-gray-300">
+                    {blog?.blogTitle}
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    {blog?.blogDetails?.slice(0, 100)}...
+                  </p>
                   <Link
                     className="inline-block hover:text-gray-900 hover:font-bold"
                     to={`/blogs/${blog?._id}`}
@@ -44,10 +53,9 @@ const PopularBlogs = () => {
               </div>
             </div>
           ))}
-        </div>
-      </>
-      
-    );
+      </div>
+    </>
+  );
 };
 
 export default PopularBlogs;
